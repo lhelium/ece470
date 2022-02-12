aa3ec92bb6d5af870fb25d2a990823bf57ce1aa4
Laura He <44853479+lhelium@users.noreply.github.com>
2022-02-11 21:56:48 -0500

    Updated comments

--- lab1/inverse.m
+++ lab1/inverse.m
@@ -1,6 +1,7 @@
 function q = inverse(H, myrobot)
-    elbow = 1;
+    elbow = 1;  % +1 for elbow up, -1 for elbow down
     
+    % Extract parameters 
     a = myrobot.a;
     d = myrobot.d;
     
@@ -8,40 +9,48 @@ function q = inverse(H, myrobot)
     Rd = H(1:3, 1:3);
     o_d = H(1:3, 4);
     
+    % calculate origin of wrist center frame
     o_c = o_d - Rd*[0 0 d(6)]';
     
     xc = o_c(1);
     yc = o_c(2);
     zc = o_c(3);
     
+    % Calculate theta 1
     theta_1 = atan2(yc, xc) - atan2(-d(2), real(sqrt(xc^2 + yc^2 - d(2)^2)));    
 
+    % Calculate sin(theta_3) = D
     D = (xc^2 + yc^2 - d(2)^2 + (zc - d(1))^2 - a(2)^2 - d(4)^2)/(2 * a(2) * d(4));
+    % Calculate theta_3
     theta_3 = atan2(D, elbow*real(sqrt(1-D^2)));
     
+    % Calculate theta_2
     theta_2 = atan2(zc - d(1), real(sqrt(xc^2 + yc^2 - d(2)^2))) - atan2(-d(4)*cos(theta_3), a(2) + d(4)*D);
     
-    %compute R_3_0
+    % Compute R_3_0 with same method and forward kinematics but with
+    % Rotational matrices instead of homogenous matrices
     R = eye(3, 3);
-    thetas = [theta_1 theta_2 theta_3];
-    for i = 1:3 % iterate through the 6 joints
+    thetas = [theta_1 theta_2 theta_3]; 
+    for i = 1:3 % iterate through the 3 joints
         theta = thetas(i);
         alpha = myrobot.alpha(i);
         
-        R_i = [cos(theta) -sin(theta)*cos(alpha)   sin(theta)*sin(alpha);
-                sin(theta) cos(theta)*cos(alpha)  -cos(theta)*sin(alpha);
-                0           sin(alpha)                  cos(alpha)
-               ];
+        % Define Rotational Matrix
+        R_i = [ cos(theta) -sin(theta)*cos(alpha)   sin(theta)*sin(alpha);
+                sin(theta)  cos(theta)*cos(alpha)  -cos(theta)*sin(alpha);
+                    0            sin(alpha)                  cos(alpha)];
         R = R * R_i;
     end
     
     R_3_0 = R;
     
-    % calculate R_6_3: R_6_3 = R_3_0T * Rd
+    % Calculate R_6_3: R_6_3 = R_3_0T * Rd
     R_6_3 = R_3_0' * Rd;
     M = R_6_3;
     
-    if (M(1, 3)^2 + M(2, 3)^2) ~= 0
+    if (M(1, 3)^2 + M(2, 3)^2) ~= 0  % check that it is not a singularity
+        % thetas 4, 5, and 6 correspond to the ZYZ Euler Angles, use the
+        % equations derrived for the Euler angles
         theta_4 = atan2(elbow * M(2, 3), elbow * M(1, 3));
         theta_5 = atan2(elbow * real(sqrt(1-M(3, 3)^2)), elbow * M(3, 3));
         theta_6 = atan2(elbow * M(3, 2), -1*elbow * M(3, 1));
--- lab1/lab1.m
+++ lab1/lab1.m
@@ -1,15 +1,18 @@
 % Lab 1
-DH = [0 76 0 pi/2;
-       0 -23.65 43.23 0;
-       0 0 0 pi/2;
-       0 43.18 0 -pi/2;
-       0 0 0 pi/2;
-       0 20 0 0];
+% Define base DH matrix of robot representing the 6 frames of the robot
+% (angles are in radians and lengths are cm)
+DH = [0  76     0    pi/2;
+      0 -23.65 43.23 0;
+      0   0     0    pi/2;
+      0  43.18  0   -pi/2;
+      0   0     0    pi/2;
+      0  20     0    0];
 
-%4.1 Definition of Robot Structure
+% 4.1 Definition of Robot Structure
 myrobot = mypuma560(DH);
 
-%4.2 Plotting the Trajectory
+% 4.2 Plotting the Trajectory
+% Define changing theta vectors
 theta_1_range = linspace(0, pi, 200);
 theta_2_range = linspace(0, pi/2, 200);
 theta_3_range = linspace(0, pi, 200);
@@ -17,47 +20,61 @@ theta_4_range = linspace(pi/4, 3/4*pi, 200);
 theta_5_range = linspace(-pi/3, pi/3, 200);
 theta_6_range = linspace(0, 2*pi, 200);
 
+% define movement matrix
 q = [theta_1_range' theta_2_range' theta_3_range' theta_4_range' theta_5_range' theta_6_range'];
 
+% Plot robot evolution corresponding to the specified joint angles
 figure(1)
 plot(myrobot, q);
 
-%4.3 Forward Kinematics
+% 4.3 Forward Kinematics
+% initialize empty matrix for storing the origin of the end effector frame
+% as the robot evolves
 o = zeros(200, 3);
-for i = 1:200
-    H = forward(q(i, :), myrobot);
-    o(i, :) = H(1:3, 4);
+
+for i = 1:200  % iterate throught the angles
+    H = forward(q(i, :), myrobot);  % calculate the homogeneous transformation
+    o(i, :) = H(1:3, 4);  % extract the coordinates of origin from the matrix
 end
-  
+
+% Plot robot evolution corresponding to the specified joint angles
+% Plot the trajectory of the end effector
 figure(2)
 plot3(o(:, 1), o(:, 2), o(:, 3), "r");
 hold on 
 plot(myrobot, q)
 
-%4.4 Inverse Kinematics
+% 4.4 Inverse Kinematics
+% Verify that inverse.m function works as expected
 H_test = [cos(pi/4) -sin(pi/4) 0 20; sin(pi/4) cos(pi/4) 0 23; 0 0 1 15; 0 0 0 1];
 q_test = inverse(H_test, myrobot);
 q_exp =[ -0.0331 -1.0667 1.0283 3.1416 3.1032 0.8185];
 
 disp(q_test);
 
+% Check calculated solution against correct solution
 assert(norm(q_test - q_exp) < 1e-3, "inverse function incorrect!");
 
+% Define a trajectory for the end effector
 x_lin = linspace(10, 30, 100);
 y_lin = linspace(23, 30, 100);
 z_lin = linspace(15, 100, 100);
 d = [x_lin' y_lin' z_lin'];
 
+% Define rotational matrix for orientation of end effector
 rot_z_pi_4 = [cos(pi/4) -sin(pi/4) 0;
               sin(pi/4)  cos(pi/4) 0;
                  0          0      1];
+
+% Initialize matrix for storing calculated joint angles
 q_4 = zeros(100, 6);
 
 for i = 1:100
-    H_fun = [[rot_z_pi_4 d(i,:)']; 0 0 0 1];
-    q_4(i, :) = inverse(H_fun, myrobot);
+    H_fun = [[rot_z_pi_4 d(i,:)']; 0 0 0 1];  % define desired homogenous matrix for end effector
+    q_4(i, :) = inverse(H_fun, myrobot);  % save joint angles
 end
 
+% Plot robot evolution corresponding to the specified joint angles
 figure(3)
 plot3(d(:, 1), d(:, 2), d(:, 3), "r");
 hold on 
